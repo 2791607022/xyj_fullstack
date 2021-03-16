19d3d4afc8817fe470dfe2bbea6545ba329ad625
2791607022 <2791607022@qq.com>
2021-03-16 21:14:06 +0800

    serverless学习

---
+++ serverless/my_express/app.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+app.get('/',(req,res)=>{
+    console.log('Home')
+    res.send('hello world')
+})
+// app.listen(3003,()=>{
+
+// })
+module.exports=app//commonjs
