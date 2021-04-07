3e49dc4e8df0bafbe84b1fd134aa095c2653af76
2791607022 <2791607022@qq.com>
2021-04-07 22:45:32 +0800

    vue3.0-first

---
+++ vue/vue3-demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <List/>
+  <todoList/>
+</template>
+
+<script>
+import List from './components/list.vue'
+import todoList from './components/todoList.vue'
+export default {
+  name: 'App',
+  components: {
+   List,
+   todoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue3-demo/src/components/list.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <h1>{{count}}*2={{double}}</h1>
+ <h2>{{num}}</h2>
+  <button @click="add"> 累加</button>
+</div>
+</template>
+
+<script>
+import {reactive, computed, ref, onMounted, toRef} from 'vue'//响应式方法 引用类型
+export default {
+    
+    setup(){
+      
+        const {state,double}=useCounter(1)
+
+        const  num=ref(2)//响应式 原型类型
+
+        function add(){
+            console.log(111)
+            state.count++
+        }
+    
+       onMounted(()=>{
+           console.log('xxsisj')
+       })
+        return{
+            ...toRef(state),
+            state,
+            add,
+            double,
+            num
+        }
+    } 
+}
+function useCounter(count){
+            const state=reactive({ count})
+            const double=computed(()=>state.count*2)//计算方法
+            return {state ,double}
+        }
+</script>
+
+<style>
+
+</style>
