5af2a9d7d3d29514ccd6f62fe9c48c02fd138550
2791607022 <2791607022@qq.com>
2021-04-10 22:02:02 +0800

    go初学

---
+++ go/range/main.go
@@ -0,0 +1,24 @@
+//模块
+package main
+import "fmt"
+func main()  {
+	nums :=[] int{2,3,4}
+	sum :=0
+	for _, num :=range nums{
+		//fmt.Println("index:",index)
+		sum+=num
+	}
+	fmt.Println("sum:",sum)
+	for i, num :=range nums  {//  {需要与 for,if,函数等语句同一行。for中第一个参数为数组下标，第二个参数为数组中的值。
+		if num==3{
+			fmt.Println("index:",i)
+		}
+	}
+	//js json object hashMAp 键值
+	kvs := map[string]string{"a":"apple",
+"b":"banana"}
+    for k, v :=range kvs{
+	fmt.Printf("%s -> %s\n",k,v)}
+	for i, c :=range"go"{fmt.Println(i,c)}
+  
+}
